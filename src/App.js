0999dc98b151014f7b917a518660ee5e0ddffab8
Anil_Ramcharran <anil.gitme@gmail.com>
2021-02-04 21:36:13 -0500

    Mvp! refactored the app using useContext hook and also stretch

--- src/App.js
+++ src/App.js
@@ -18,10 +18,14 @@ function App() {
 		return (item.id && setCart([...cart, item]))
 	};
 
+	const removeItem = id => {
+		setCart(cart.filter(item => id !== item.id))
+	}
+
 	return (
 		<div className="App">
 			<ProductContext.Provider value={{products, addItem}}>
-				<CartContext.Provider value={{cart}}>
+				<CartContext.Provider value={{cart, removeItem}}>
 				<Navigation cart={cart} />
 
 			{/* Routes */}
